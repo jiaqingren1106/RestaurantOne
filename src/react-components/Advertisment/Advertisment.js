c509ecd71fd3153b49c9eb9bd2d01274c71fb099
Patrick Louis <patricklouisw@gmail.com>
2020-10-30 14:20:41 -0400

    Fixed bug

--- src/react-components/Advertisment/Advertisment.js
+++ src/react-components/Advertisment/Advertisment.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Advertisment.css';
-import pic from "/Users/renjiaqing/Downloads/team41/src/images/AW.png";
 
 class Advertisment extends React.Component {
   render() {
