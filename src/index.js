2216ec492af167356227bc33caa09016db793765
Yingke Wang <yingkewang.wang@mail.utoronto.ca>
2020-10-30 17:20:57 -0400

    merge with the head

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import FirstPage from './pages/FirstPage';
 import SecondPage from './pages/SecondPage';
+import SignIn from "./fontPages/SignIn";
 
 ReactDOM.render(
+
   <React.StrictMode>
-    <FirstPage />
+    <SignIn/>
   </React.StrictMode>,
-  document.getElementById('root')
+    document.getElementById('root')
 );
+
 
