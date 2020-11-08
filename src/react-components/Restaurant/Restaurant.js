f45b2120a67128a4bd5a1d4b6a02627c27409298
renjiaq1 <jiaqing.ren@mail.utoronto.ca>
2020-11-08 16:02:11 -0500

    reformat firstpage

--- src/react-components/Restaurant/Restaurant.js
+++ src/react-components/Restaurant/Restaurant.js
@@ -6,10 +6,18 @@ import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Restaurant extends React.Component {
+  
+  useMeWhenOnClick = (props) => {
+    console.log(1)
+}
+
   render() {
     return (
       <Card id="restaurant1" className="restaurantCard">
-        <Card.Img className="restaurantPic" variant="top" src={this.props.image} />
+        <button onClick={this.useMeWhenOnClick}>
+          <Card.Img className="restaurantPic" variant="top" src={this.props.image} />
+
+        </button>
         <Card.Body className="restaurantBody">
           <Card.Title className="restaurantTitle">{this.props.name}</Card.Title>
           <Card.Text>
