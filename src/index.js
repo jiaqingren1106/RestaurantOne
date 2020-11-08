6c2bb92727f7fc09d89991f07328c318ada643dd
Patrick Louis <patricklouisw@gmail.com>
2020-11-08 12:37:51 -0500

    Comment Testing Components

--- src/index.js
+++ src/index.js
@@ -9,14 +9,14 @@ import {Provider} from "react-redux";
 
 const logger = createLogger()
 const store = createStore(appState, applyMiddleware(logger))
-import FirstPage from './pages/FirstPage';
-import SecondPage from './pages/SecondPage';
-import SignIn from "./fontPages/SignIn";
-import AdminPage from "./Admin/AdminPage"
-import PostsTable from "./Admin/admin-components/PostsTable"
-import UserTable from "./Admin/admin-components/UserTable"
-import RestaurantOwnerTable from "./Admin/admin-components/RestaurantOwnerTable"
-import ReviewsTable from "./Admin/admin-components/ReviewsTable"
+// import FirstPage from './pages/FirstPage';
+// import SecondPage from './pages/SecondPage';
+// import SignIn from "./fontPages/SignIn";
+// import AdminPage from "./Admin/AdminPage"
+// import PostsTable from "./Admin/admin-components/PostsTable"
+// import UserTable from "./Admin/admin-components/UserTable"
+// import RestaurantOwnerTable from "./Admin/admin-components/RestaurantOwnerTable"
+// import ReviewsTable from "./Admin/admin-components/ReviewsTable"
 
 
 ReactDOM.render(
