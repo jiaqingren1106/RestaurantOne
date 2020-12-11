462c79059990cf2155bb9f8cc68c68680079a79d
Zhiyuan Sun <32525238+ZhiYuanS@users.noreply.github.com>
2020-12-11 06:42:30 -0500

    Connect Register page to the backend

--- src/Action/imageAction.js
+++ src/Action/imageAction.js
@@ -2,6 +2,37 @@
 import ENV from '../config.js'
 const API_HOST = ENV.api_host
 
+
+export const createImage = (file, setimageId) => {
+    const url = `${API_HOST}/image`
+
+    console.log(file)
+    const imageData = new FormData(file);
+    const request = new Request(url,
+        {
+            method:"post",
+            body:imageData
+        })
+        
+    console.log(request)
+    fetch(request)
+        .then(res => {
+            if (res.status === 200) {
+                return res.json()
+            } else {
+                alert("Could not get description");
+            }
+        })
+        .then(json => {
+            // console.log(json)
+            setimageId(json["_id"])
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
+
+
 export const getImage = (Comp, image_id) => {
     const url = `${API_HOST}/image/${image_id}`
     const request = new Request(url,
